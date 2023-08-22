8b0ac91d746ece6dd88a09c46104ded07c6eb94c
najibjul <muhammadnajibj@gmail.com>
2023-08-22 22:40:03 +0700

    Penambahan laravel excel, laravel pdf, laravel sweetalert, laravel PWA

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -226,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
     ],
 
 ];
